a36383660a45d121b84259eb4286e81618691683
Martin Casares <casaresrm@gmail.com>
2024-01-16 09:38:18 -0300

    Add mini-component nav-hero

--- src/components/listComponents/ListComponents.jsx
+++ src/components/listComponents/ListComponents.jsx
@@ -1,23 +1,26 @@
 import './listComponents.css';
-import { MiniCard } from './miniCard/MiniCard';
+
 import { MiniForm } from './miniForm/MiniForm';
+import { ListUsers } from './listUsers/ListUsers';
+import { MiniNav } from './miniNav/MiniNav';
 
 export const ListComponents = ({ colors }) => {
+	const components = [MiniNav, ListUsers, MiniForm];
 	return (
 		<div>
 			<section className="mini-layout">
-				{colors?.map((color, index) => (
-					<>
+				{colors?.map((color, index) => {
+					const Component = components[index % components.length];
+					return (
 						<div
 							key={index}
 							className="mini-box"
 							style={{ background: color, width: '100%', height: '25rem' }}
 						>
-							<MiniForm colors={colors} />
-							{/* <MiniCard colors={colors} /> */}
+							<Component colors={colors} />
 						</div>
-					</>
-				))}
+					);
+				})}
 			</section>
 		</div>
 	);
---
+++ src/components/listComponents/miniNav/MiniNav.jsx
@@ -0,0 +1,65 @@
+import './miniNav.css';
+import { FaGithub } from 'react-icons/fa6';
+import { FaLinkedin } from 'react-icons/fa6';
+import { FaTwitter } from 'react-icons/fa6';
+
+export const MiniNav = ({ colors }) => {
+	return (
+		<div className="mini-nav">
+			<div className="nav-wrapper">
+				<div className="nav-content">
+					<strong className="logo" style={{ color: colors[3] }}>
+						Logo
+					</strong>
+					<ul>
+						<li>
+							<a href="" className="menu-item" style={{ color: colors[1] }}>
+								Home
+							</a>
+						</li>
+						<li>
+							<a href="" className="menu-item" style={{ color: colors[1] }}>
+								Blog
+							</a>
+						</li>
+						<li>
+							<a href="" className="menu-item" style={{ color: colors[1] }}>
+								Contact
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="hero">
+				<p className="hero-title" style={{ color: colors[4] }}>
+					Hello I'm
+					<p style={{ color: colors[5] }}>frontend developer...</p>
+				</p>
+				<button
+					className="hero-button"
+					style={{
+						background: colors[5],
+						border: `2.5px solid ${colors[0]}`,
+						color: colors[2],
+					}}
+				>
+					<span className="followCard-text">download cv</span>
+				</button>
+				<div className="hero-icons">
+					<a href="">
+						<FaGithub size={50} style={{ color: colors[4], marginRight: '10px' }} />
+					</a>
+					<a href="">
+						<FaLinkedin
+							size={50}
+							style={{ color: colors[4], marginRight: '10px' }}
+						/>
+					</a>
+					<a href="">
+						<FaTwitter size={50} style={{ color: colors[4], marginRight: '10px' }} />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
